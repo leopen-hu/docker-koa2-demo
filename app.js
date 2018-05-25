8434b7103f6799abe4e99f5d63bbe62d87a62d83
lipeng.hu@itdev.xiangguang.com <lipeng.hu@itdev.xiangguang.com>
2018-05-25 16:24:27 +0800

    [leopen] 修改了数据库的链接和启动

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const Koa = require('koa')
-
 const Article = require('./models/article')
+const dbInit = require('./models/index')
 
 const app = new Koa()
-
 app.use(async (ctx, next) => {
   await next()
   console.log('ctx.request:', ctx.request)
   var result;
+  // var testArticle = new Article({
+  //   title: 'String',
+  //   content: 'String'
+  // })
+  // await testArticle.save((err, x_result) => {
+  //   if (err) return console.error(err);
+  //   console.log(x_result);
+  // })
   await Article.find((err, _result) => {
+    if (err) return console.error(err);
     result = _result;
-    console.log(result);
+    console.log('find articles', result);
   });
   ctx.response.body = result;
 })
--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const Article = require('./article')
 
-mongoose.connect('mongodb://127.0.0.1/test')
+mongoose.connect('mongodb://101.132.171.233:27017/test')
 const db = mongoose.connection
 
 db.on('error', console.error.bind(console, 'connection error:'));
